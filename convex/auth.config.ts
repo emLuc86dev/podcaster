2fa44d5eb030672038641d0d626aebfe62ca1148
emLuc86dev <esmcode18@gmail.com>
2024-06-22 18:50:48 -0500

    installing auth clerk and cofig with convex
    docs: https://docs.convex.dev/auth/clerk

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const authConfig = {
+  providers: [
+    {
+      domain: "https://tops-polliwog-42.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
+
+export default authConfig;
---
+++ convex/http.ts
@@ -0,0 +1,1 @@
+// create a clerk user -> podcast -> audio -> image -> voice -> audio -> views
